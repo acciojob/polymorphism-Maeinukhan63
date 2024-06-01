fdb9eaa87e8e8d97208b539ef8ec05db62bbac92
Maeinukhan <khanmaeinuddin19@gmail.com>
2024-06-01 13:02:11 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    public static void main (String args[]){
+        product p=new product();
+        System.out.pintln(p.product(2,3));
+        System.out.println(p.product(1,2,3));
+        System.out.println(p.product(2.0,3.0));
+    }
+
+    public static class product{
+        public int product(int x, int y) {
+            return x+y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x+y+z;
+        }
+
+        public double product(double x, double y) {
+            return x+y;
+        }
+    }
 }
